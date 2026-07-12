095736cb632763df9e66df0cf21ebad41aa8f589
mshahwaiz-ali <241361889+mshahwaiz-ali@users.noreply.github.com>
2026-07-12 23:10:31 +0500

    export Phase 3 structure API

---
+++ src/apex/structure/__init__.py
@@ -0,0 +1,56 @@
+"""Deterministic market-structure engine."""
+
+from apex.structure.analysis import analyze_structure
+from apex.structure.breaks import detect_changes_of_character, detect_structure_breaks
+from apex.structure.contracts import (
+    BreakDirection,
+    BreakQuality,
+    ChangeOfCharacter,
+    ComparisonPolicy,
+    ConfirmationStatus,
+    LevelRole,
+    LevelStatus,
+    PivotStatus,
+    RangeBreakoutState,
+    RangeStructure,
+    StructureAnalysisResult,
+    StructureBreak,
+    StructureLevel,
+    SwingPoint,
+    SwingType,
+    TrendAnalysis,
+    TrendDirection,
+    TrendEvidence,
+)
+from apex.structure.levels import derive_structure_levels
+from apex.structure.ranges import detect_range
+from apex.structure.swings import detect_swings
+from apex.structure.trend import classify_trend
+
+__all__ = [
+    "BreakDirection",
+    "BreakQuality",
+    "ChangeOfCharacter",
+    "ComparisonPolicy",
+    "ConfirmationStatus",
+    "LevelRole",
+    "LevelStatus",
+    "PivotStatus",
+    "RangeBreakoutState",
+    "RangeStructure",
+    "StructureAnalysisResult",
+    "StructureBreak",
+    "StructureLevel",
+    "SwingPoint",
+    "SwingType",
+    "TrendAnalysis",
+    "TrendDirection",
+    "TrendEvidence",
+    "analyze_structure",
+    "classify_trend",
+    "derive_structure_levels",
+    "detect_changes_of_character",
+    "detect_range",
+    "detect_structure_breaks",
+    "detect_swings",
+]
