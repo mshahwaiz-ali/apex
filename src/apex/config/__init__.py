ca90789ba5c250300699c3a56c800151d152c467
mshahwaiz-ali <241361889+mshahwaiz-ali@users.noreply.github.com>
2026-07-14 15:19:37 +0500

    export S2 spot structure config

--- src/apex/config/__init__.py
+++ src/apex/config/__init__.py
@@ -16,42 +16,4 @@ from apex.config.settings import (
     DEFAULT_TIMEFRAME_RESAMPLING_SOURCES,
     DEFAULT_TIMEFRAME_ROLES,
     FileSettings,
-    load_settings,
-)
-from apex.config.spot import (
-    SpotAllocationConfig,
-    SpotEntryConfig,
-    SpotExitConfig,
-    SpotProductConfig,
-    load_spot_product_config,
-)
-from apex.config.strategy_approval import (
-    StrategyApprovalConfig,
-    StrategyApprovalRule,
-    StrategyQualityClass,
-    load_strategy_approval_config,
-)
-
-__all__ = [
-    "DEFAULT_STRATEGY_ROUTING",
-    "DEFAULT_TIMEFRAME_MAX_STALENESS_SECONDS",
-    "DEFAULT_TIMEFRAME_RESAMPLING_SOURCES",
-    "DEFAULT_TIMEFRAME_ROLES",
-    "AccountPoliciesConfig",
-    "FileSettings",
-    "FuturesExecutionCostConfig",
-    "FuturesProductConfig",
-    "RiskModeDefaults",
-    "SpotAllocationConfig",
-    "SpotEntryConfig",
-    "SpotExitConfig",
-    "SpotProductConfig",
-    "StrategyApprovalConfig",
-    "StrategyApprovalRule",
-    "StrategyQualityClass",
-    "load_account_policies_config",
-    "load_futures_product_config",
-    "load_settings",
-    "load_spot_product_config",
-    "load_strategy_approval_config",
-]
+    load_settings
