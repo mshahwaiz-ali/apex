2da885ab6410d247a4bb38fe82fb6fd4bc1eb420
mshahwaiz-ali <241361889+mshahwaiz-ali@users.noreply.github.com>
2026-07-16 13:16:37 +0500

    export funded provider registry contracts

--- src/apex/funded/__init__.py
+++ src/apex/funded/__init__.py
@@ -12,6 +12,21 @@ from apex.funded.history_readiness_artifact_verification import (
     funded_history_readiness_artifact_source_verification_payload,
     verify_funded_history_readiness_artifact_sources,
 )
+from apex.funded.provider_limits_persistence import (
+    load_funded_provider_limits_registry,
+    validate_provider_preset_against_policy,
+    write_funded_provider_limits_registry,
+)
+from apex.funded.provider_limits_registry import (
+    PROVIDER_LIMITS_REGISTRY_SCHEMA_VERSION,
+    DrawdownModel,
+    FundedProviderLimitPreset,
+    FundedProviderLimitsRegistry,
+)
+from apex.funded.provider_readiness_input import (
+    prepare_funded_readiness_input,
+    write_funded_readiness_input,
+)
 from apex.funded.readiness import (
     FundedProviderLimits,
     FundedReadinessReason,
@@ -35,9 +50,13 @@ from apex.funded.readiness_artifact_verification import (
 __all__ = [
     "FUNDED_HISTORY_READINESS_ARTIFACT_SCHEMA_VERSION",
     "FUNDED_READINESS_ARTIFACT_SCHEMA_VERSION",
+    "PROVIDER_LIMITS_REGISTRY_SCHEMA_VERSION",
+    "DrawdownModel",
     "FundedHistoryReadinessArtifactSourceStatus",
     "FundedHistoryReadinessArtifactSourceVerification",
+    "FundedProviderLimitPreset",
     "FundedProviderLimits",
+    "FundedProviderLimitsRegistry",
     "FundedReadinessArtifactSourceStatus",
     "FundedReadinessArtifactSourceVerification",
     "FundedReadinessReason",
@@ -50,8 +69,13 @@ __all__ = [
     "funded_readiness_artifact_source_verification_payload",
     "load_and_verify_funded_history_readiness_artifact",
     "load_and_verify_funded_readiness_artifact",
+    "load_funded_provider_limits_registry",
+    "prepare_funded_readiness_input",
+    "validate_provider_preset_against_policy",
     "verify_funded_history_readiness_artifact_sources",
     "verify_funded_readiness_artifact_sources",
     "write_funded_history_readiness_artifact",
+    "write_funded_provider_limits_registry",
     "write_funded_readiness_artifact",
+    "write_funded_readiness_input",
 ]
