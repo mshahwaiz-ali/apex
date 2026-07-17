d76ddb595bdd693c31a8b67e2c983691e3bee19e
Muhammad Shahwaiz Ali <mshahwaiz-ali@users.noreply.github.com>
2026-07-17 17:00:52 +0500

    remove dead artifact verification ceremony

--- src/apex/funded/__init__.py
+++ src/apex/funded/__init__.py
@@ -1,17 +1,5 @@
 """Public funded-readiness API."""
 
-from apex.funded.history_readiness_artifact import (
-    FUNDED_HISTORY_READINESS_ARTIFACT_SCHEMA_VERSION,
-    build_funded_history_readiness_artifact,
-    load_and_verify_funded_history_readiness_artifact,
-    write_funded_history_readiness_artifact,
-)
-from apex.funded.history_readiness_artifact_verification import (
-    FundedHistoryReadinessArtifactSourceStatus,
-    FundedHistoryReadinessArtifactSourceVerification,
-    funded_history_readiness_artifact_source_verification_payload,
-    verify_funded_history_readiness_artifact_sources,
-)
 from apex.funded.plan_eligibility import (
     FundedPlanEligibility,
     FundedPlanEligibilityReason,
@@ -69,30 +57,14 @@ from apex.funded.readiness import (
     ManualExecutionChecklist,
     evaluate_funded_readiness,
 )
-from apex.funded.readiness_artifact import (
-    FUNDED_READINESS_ARTIFACT_SCHEMA_VERSION,
-    build_funded_readiness_artifact,
-    load_and_verify_funded_readiness_artifact,
-    write_funded_readiness_artifact,
-)
-from apex.funded.readiness_artifact_verification import (
-    FundedReadinessArtifactSourceStatus,
-    FundedReadinessArtifactSourceVerification,
-    funded_readiness_artifact_source_verification_payload,
-    verify_funded_readiness_artifact_sources,
-)
 
 __all__ = [
-    "FUNDED_HISTORY_READINESS_ARTIFACT_SCHEMA_VERSION",
     "FUNDED_PLAN_AUDIT_SCHEMA_VERSION",
     "FUNDED_PLAN_INDEX_SCHEMA_VERSION",
     "FUNDED_PLAN_PACKAGE_SCHEMA_VERSION",
     "FUNDED_PLAN_REPRODUCTION_SCHEMA_VERSION",
-    "FUNDED_READINESS_ARTIFACT_SCHEMA_VERSION",
     "PROVIDER_LIMITS_REGISTRY_SCHEMA_VERSION",
     "DrawdownModel",
-    "FundedHistoryReadinessArtifactSourceStatus",
-    "FundedHistoryReadinessArtifactSourceVerification",
     "FundedPlanAuditSummary",
     "FundedPlanEligibility",
     "FundedPlanEligibilityReason",
@@ -107,38 +79,26 @@ __all__ = [
     "FundedProviderLimitPreset",
     "FundedProviderLimits",
     "FundedProviderLimitsRegistry",
-    "FundedReadinessArtifactSourceStatus",
-    "FundedReadinessArtifactSourceVerification",
     "FundedReadinessReason",
     "FundedReadinessReport",
     "ManualExecutionChecklist",
     "ProviderPolicyBinding",
     "bind_provider_policy",
-    "build_funded_history_readiness_artifact",
     "build_funded_plan_audit_summary",
     "build_funded_plan_evidence_package",
     "build_funded_plan_package_index",
-    "build_funded_readiness_artifact",
     "canonical_sha256",
     "evaluate_funded_plan_eligibility",
     "evaluate_funded_readiness",
-    "funded_history_readiness_artifact_source_verification_payload",
-    "funded_readiness_artifact_source_verification_payload",
-    "load_and_verify_funded_history_readiness_artifact",
     "load_and_verify_funded_plan_evidence_package",
     "load_and_verify_funded_plan_package_index",
-    "load_and_verify_funded_readiness_artifact",
     "load_funded_provider_limits_registry",
     "prepare_funded_readiness_input",
     "validate_provider_preset_against_policy",
-    "verify_funded_history_readiness_artifact_sources",
     "verify_funded_plan_evidence_package",
     "verify_funded_plan_package_reproduction",
-    "verify_funded_readiness_artifact_sources",
-    "write_funded_history_readiness_artifact",
     "write_funded_plan_evidence_package",
     "write_funded_plan_package_index",
     "write_funded_provider_limits_registry",
-    "write_funded_readiness_artifact",
     "write_funded_readiness_input",
 ]
